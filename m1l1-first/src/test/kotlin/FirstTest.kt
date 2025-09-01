e9c682d8da8f20b44b669ac2dbdfed9948c438e2
Samorodov Pavel <paul.g@amzscout.net>
2025-09-01 22:01:04 +0700

    m1l1-first add test

---
+++ m1l1-first/src/test/kotlin/FirstTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FirstTest {
+    @Test
+    fun testFirst() {
+        assertEquals(5, 2 + 3)
+    }
+}
